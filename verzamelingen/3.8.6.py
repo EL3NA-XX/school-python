6c10370f2e621836cae27a61ade1fa7d10fbb93b
EL3NA <beatricemanaila@gmail.com>
2026-03-18 11:46:02 +0100

    Delete verzamelingen directory

--- verzamelingen/3.8.6.py
+++
@@ -1,7 +0,0 @@
-def gemeenschappelijk(lijst1,lijst2):
-    gem = set()
-    for elem in lijst1:
-        if elem in lijst2:
-            gem.add(elem)
-    return gem
-print(gemeenschappelijk([1,2,3],[1,2,3,4]))
